0a8f6f8ce789af8c251934eeb9484cd46c18ff36
Johnathan Martin <mrjohnathanmartin@gmail.com>
2015-01-04 13:09:33 -0600

    Update footer.php

--- view/footer.php
+++ view/footer.php
@@ -1,4 +1,5 @@
 <?php if (!defined("IN_WALLET")) { die("u can't touch this."); } ?>
+<?php //PLEASE DO NOT REMOVE OR CHANGE THE POWERED BY LINK. THIS IS THE ONLY RECOGNITION I ASK FOR ?>
             </div>
         </div>
 
