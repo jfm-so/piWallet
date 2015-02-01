3daebb19d50992d7e77a1ea162dae7d66ba6b0c7
Johnathan Martin <admin@fitbobcat.com>
2015-02-01 16:03:22 -0600

    Update common.php
    Fixed Error where piWallet Super Administrator account was able to be accessed through admin dashboard

--- common.php
+++ common.php
@@ -33,6 +33,6 @@ $fullname = "Bitcoin"; //Website Title (Do Not include 'wallet')
 $short = "BTC"; //Coin Short (BTC)
 $blockchain_url = "http://blockchain.info/tx/"; //Blockchain Url
 
-$hide_ids = array(6); //Hide account from admin dashboard
+$hide_ids = array(1); //Hide account from admin dashboard
 $donation_address = "13jy6rHB7HMgQBoYxQQXSM7TFTZZ6CDAAZ"; //Donation Address
 ?>
