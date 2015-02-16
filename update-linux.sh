5118264487e707a73f7ab7887d9c53ce9ea518d7
Johnathan Martin <admin@fitbobcat.com>
2015-02-16 09:18:17 -0600

    Update update-linux.sh
    Added matching "

--- update-linux.sh
+++ update-linux.sh
@@ -1,6 +1,6 @@
 #Coming Soon/Beta - Do Not Use
 echo "This will delete all custom development. Your Database and config file will not be updated"
-read -p "Press [Enter] key to confirm
+read -p "Press [Enter] key to confirm"
 rm -r assets/
 rm -r classes/
 rm -r view/
