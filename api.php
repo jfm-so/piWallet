12358e4171dd0dd87dd279bd2fb7dcc708618a16
Johnathan Martin <mrjohnathanmartin@gmail.com>
2016-02-06 11:26:59 -0500

    Update api.php
    aesthetics

--- api.php
+++ api.php
@@ -1,15 +1,14 @@
 <?php
 define("IN_WALLET", true);
 include('common.php');
-$con=mysqli_connect("$db_host","$db_user","$db_pass","$db_name");
+$con = mysqli_connect("$db_host","$db_user","$db_pass","$db_name");
 $key = $_GET['key'];
-if (mysqli_connect_errno())
-{
+if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL. Make sure to edit the common.php file: " . mysqli_connect_error();
 }
 $result = mysqli_query($con,"SELECT * FROM users where secret = '$key' and authused=1");
-while($row = mysqli_fetch_array($result))
-{
+
+while($row = mysqli_fetch_array($result)) {
     $id = $row['id'];
     $username = $row['username'];
     $pin = $row['supportpin'];
@@ -19,15 +18,15 @@ while($row = mysqli_fetch_array($result))
     $apibal = $client->getBalance($username);
     $addr = $client->getAddress($username);;
 }
-if ($isadmin == 1)
-{
-$ifadmin = "true"; }
-else {
-$ifadmin = "false"; }
-if ($twofactoren == 1)
-{
-$iftwofactor = "true"; }
-else {
-$iftwofactor = "false";}
+if ($isadmin == 1) {
+    $ifadmin = "true";
+} else {
+    $ifadmin = "false";
+}
+if ($twofactoren == 1) {
+    $iftwofactor = "true";
+} else {
+    $iftwofactor = "false";
+}
 echo json_encode(array("id" => "$id", "username" => "$username", "balance" => "$apibal", "addresses" => "$addr", "Support Pin" => "$pin", "Two Factor Enabled"=> "$iftwofactor", "admin" => "$ifadmin"));
 ?>
