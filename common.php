1e552dedbb0c621701fbd156e6b529f5a8daf591
MystPhysX <mystphysx@gmail.com>
2016-10-18 20:14:48 +0530

    Language fix and more languages added
    Added languages:
    - Arabic
    - German
    - French
    - Japanese
    - Korean
    - Dutch
    - Russian
    - Turkish
    - Vietnamese

--- common.php
+++ common.php
@@ -2,6 +2,43 @@
 session_start();
 header('Cache-control: private'); // IE 6 FIX
 
+define("WITHDRAWALS_ENABLED", true); //Disable withdrawals during maintenance
+
+include('jsonRPCClient.php');
+include('classes/Client.php');
+include('classes/User.php');
+
+// function by zelles to modify the number to bitcoin format ex. 0.00120000
+function satoshitize($satoshitize) {
+   return sprintf("%.8f", $satoshitize);
+}
+
+// function by zelles to trim trailing zeroes and decimal if need
+function satoshitrim($satoshitrim) {
+   return rtrim(rtrim($satoshitrim, "0"), ".");
+}
+
+$server_url = "/";  // website url
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "password";
+$db_name = "wallet";
+
+$rpc_host = "127.0.0.1";
+$rpc_port = "8332";
+$rpc_user = "bitcoinrpc";
+$rpc_pass = "Cp68nBkCAADKkskaKSskaDKdmSYLtLJ";
+
+$fullname = "Bitcoin"; //Website Title (Do Not include 'wallet')
+$short = "BTC"; //Coin Short (BTC)
+$blockchain_url = "http://blockchain.info/tx/"; //Blockchain Url
+$support = "support@yourwebsite.com"; //Your support eMail
+$hide_ids = array(1); //Hide account from admin dashboard
+$donation_address = "13jy6rHB7HMgQBoYxQQXSM7TFTZZ6CDAAZ"; //Donation Address
+
+$fee = "0.001"; //Set a fee to prevent negitive balances. WARNING: This function is incomplete. Setting a fee will show a negitve balance for all new user accounts.
+
 if(isSet($_GET['lang']))
 {
 $lang = $_GET['lang'];
@@ -57,6 +94,42 @@ switch ($lang) {
   $lang_file = 'lang.tgl.php';
   break;
 
+  case 'rus':
+  $lang_file = 'lang.rus.php';
+  break;
+
+  case 'nld':
+  $lang_file = 'lang.nld.php';
+  break;
+
+  case 'fra':
+  $lang_file = 'lang.fra.php';
+  break;
+
+  case 'deu':
+  $lang_file = 'lang.deu.php';
+  break;
+
+  case 'tur':
+  $lang_file = 'lang.tur.php';
+  break;
+
+  case 'vie':
+  $lang_file = 'lang.vie.php';
+  break;
+
+  case 'jpn':
+  $lang_file = 'lang.jpn.php';
+  break;
+
+  case 'kor':
+  $lang_file = 'lang.kor.php';
+  break;
+
+  case 'ara':
+  $lang_file = 'lang.ara.php';
+  break;
+
   default:
   $lang_file = 'lang.en.php';
 
@@ -64,41 +137,4 @@ switch ($lang) {
 
 include_once 'languages/'.$lang_file;
 
-
-define("WITHDRAWALS_ENABLED", true); //Disable withdrawals during maintenance
-
-include('jsonRPCClient.php');
-include('classes/Client.php');
-include('classes/User.php');
-
-// function by zelles to modify the number to bitcoin format ex. 0.00120000
-function satoshitize($satoshitize) {
-   return sprintf("%.8f", $satoshitize);
-}
-
-// function by zelles to trim trailing zeroes and decimal if need
-function satoshitrim($satoshitrim) {
-   return rtrim(rtrim($satoshitrim, "0"), ".");
-}
-
-$server_url = "/";  // website url
-
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "password";
-$db_name = "wallet";
-
-$rpc_host = "127.0.0.1";
-$rpc_port = "8332";
-$rpc_user = "bitcoinrpc";
-$rpc_pass = "Cp68nBkCAADKkskaKSskaDKdmSYLtLJ";
-
-$fullname = "Bitcoin"; //Website Title (Do Not include 'wallet')
-$short = "BTC"; //Coin Short (BTC)
-$blockchain_url = "http://blockchain.info/tx/"; //Blockchain Url
-$support = "support@yourwebsite.com"; //Your support eMail
-$hide_ids = array(1); //Hide account from admin dashboard
-$donation_address = "13jy6rHB7HMgQBoYxQQXSM7TFTZZ6CDAAZ"; //Donation Address
-
-$fee = "0.001"; //Set a fee to prevent negitive balances. WARNING: This function is incomplete. Setting a fee will show a negitve balance for all new user accounts. 	
 ?>
