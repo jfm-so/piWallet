2bdd26129a13f690b15bafb08d06f1162708d984
Johnathan Martin <jmartin@fitbobcat.com>
2018-02-02 22:09:46 -0600

    Update settings-example.php

--- settings-example.php
+++ settings-example.php
@@ -17,7 +17,7 @@ $short = "BTC"; //Coin Short (BTC)
 $blockchain_tx_url = "http://blockchain.info/tx/"; //Blockchain Url
 $support = "support@yourwebsite.com"; //Your support eMail
 $hide_ids = array(1); //Hide account from admin dashboard
-$donation_address = "13jy6rHB7HMgQBoYxQQXSM7TFTZZ6CDAAZ"; //Donation Address
+$donation_address = ""; //Donation Address
 
 $reserve = "0"; //This fee acts as a reserve. The users balance will display as the balance in the daemon minus the reserve. We don't reccomend setting this more than the Fee the daemon charges.
 
