1459dca0d4a875920c9788fe871e3da20cdc5ac8
George Yovchev <georgigeorgi73@gmail.com>
2018-05-17 13:37:45 +0300

    Bulgarian language added
    Bulgarian language added

---
+++ languages/lang.bgn.php
@@ -0,0 +1,44 @@
+<?php
+/*
+------------------
+Language: Bulgarian
+------------------
+*/
+$lang = array();
+//Home Page
+$lang['PAGE_HEADER'] = 'Добре дошъл в ' . $fullname . ' wallet!';
+$lang['FORM_LOGIN'] = 'Влез';
+$lang['FORM_USER'] = 'Потребителско име';
+$lang['FORM_PASS'] = 'Парола';
+$lang['FORM_PASSCONF'] = 'Потвърди паролата';
+$lang['FORM_2FA'] = '2FA Authetication Code';
+$lang['FORM_SIGNUP'] = 'Регистрация';
+$lang['FORM_CREATE'] = '...или създай нов профил:';
+// Wallet Page
+$lang['WALLET_HELLO'] = 'Здравей';
+$lang['WALLET_BALANCE'] = 'Текуща наличност:';
+$lang['WALLET_USERLINKS'] = 'Линкове на потребители:';
+$lang['WALLET_LOGOUT'] = 'Изход';
+$lang['WALLET_SUPPORT'] = 'Поддръжка';
+$lang['WALLET_2FAON'] = 'Enable 2 Factor Auth';
+$lang['WALLET_2FAOFF'] = 'Disable 2 Factor Auth';
+$lang['WALLET_PASSUPDATE'] = 'Смяна на парола:';
+$lang['WALLET_PASSUPDATEOLD'] = 'Текуща парола';
+$lang['WALLET_PASSUPDATENEW'] = 'Нова парола';
+$lang['WALLET_PASSUPDATENEWCONF'] = 'Потвърди новата парола';
+$lang['WALLET_PASSUPDATECONF'] = 'Смяна на парола';
+$lang['WALLET_SUPPORTNOTE'] = 'Моля натисни "Поддръжка" по-горе и запиши своя Ключ. Той ще бъде използван, да помогне да те идентифицираме, ако някога си забравиш паролата. Този код също така се променя всеки път щом си забравиш паролата.';
+$lang['WALLET_SEND'] = 'Изпрати:';
+$lang['WALLET_ADDRESS'] = 'Адрес';
+$lang['WALLET_AMOUNT'] = 'Количество';
+$lang['WALLET_SENDCONF'] = 'Изпрати';
+$lang['WALLET_USERADDRESSES'] = 'Твоите адреси:';
+$lang['WALLET_NEWADDRESS'] = 'Добави нов адрес';
+$lang['WALLET_QRCODE'] = 'QR Code';
+$lang['WALLET_LAST10'] = 'Последните 10 транзакции:';
+$lang['WALLET_DATE'] = 'Дата';
+$lang['WALLET_TYPE'] = 'Тип';
+$lang['WALLET_FEE'] = 'Такса';
+$lang['WALLET_CONFS'] = 'Потвърждения';
+$lang['WALLET_INFO'] = 'Информация';
+?>
