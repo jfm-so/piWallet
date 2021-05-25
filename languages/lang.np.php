00da6d3d102bbd5abe50b4cbc25eee8c0da0735a
unelmacoin <83529744+unelmacoin@users.noreply.github.com>
2021-05-25 12:01:58 +0300

    Create lang.np.php

---
+++ languages/lang.np.php
@@ -0,0 +1,50 @@
+<?php
+/*
+----------------------
+Language: Nepali/नेपाली
+----------------------
+*/
+
+$lang = array();
+
+//Home Page
+
+$lang['PAGE_HEADER'] = 'स्वागत छ ' . $fullname . ' वालेटमा!';
+$lang['FORM_LOGIN'] = 'लग - इन';
+$lang['FORM_USER'] = 'प्रयोगकर्ता नाम';
+$lang['FORM_PASS'] = 'पासवर्ड';
+$lang['FORM_PASSCONF'] = 'पासवर्ड सुनिश्चित गर्नुहोस';
+$lang['FORM_2FA'] = '2FA प्रमाणीकरण कोड';
+$lang['FORM_SIGNUP'] = 'साइन अप';
+$lang['FORM_CREATE'] = '...वा नयाँ खाता सिर्जना गर्नुहोस्:';
+
+// Wallet Page
+
+$lang['WALLET_HELLO'] = 'नमस्कार';
+$lang['WALLET_BALANCE'] = 'वर्तमान ब्यालेन्स:';
+$lang['WALLET_USERLINKS'] = 'प्रयोगकर्ता लिंकहरू:';
+$lang['WALLET_LOGOUT'] = 'बाहिर निस्कनु';
+$lang['WALLET_SUPPORT'] = 'समर्थन';
+$lang['WALLET_2FAON'] = '२ कारक प्रमाणीकरण सक्षम गर्नुहोस्';
+$lang['WALLET_2FAOFF'] = '२ कारक प्रमाणीकरण असक्षम गर्नुहोस्';
+$lang['WALLET_PASSUPDATE'] = 'तपाईंको पासवर्ड अपडेट गर्नुहोस्:';
+$lang['WALLET_PASSUPDATEOLD'] = 'वर्तमान पासवर्ड';
+$lang['WALLET_PASSUPDATENEW'] = 'नया पासवर्ड';
+$lang['WALLET_PASSUPDATENEWCONF'] = 'नयाँ पासवर्ड निश्चित गर्नुहोस्';
+$lang['WALLET_PASSUPDATECONF'] = 'पासवर्ड अपडेट गर्नुहोस्';
+$lang['WALLET_SUPPORTNOTE'] = 'कृपया माथिको समर्थनमा क्लिक गर्नुहोस् र तपाईंको कुञ्जी नोट बनाउनुहोस्। यो तपाईंलाई पहिचान गर्न मद्दतको लागि प्रयोग गरिन्छ यदि तपाईं कहिले पासवर्ड बिर्सनुभयो भने। यो कोड हरेक पल्ट परिवर्तन हुन्छ जब तपाईं आफ्नो पासवर्ड परिवर्तन गर्नुहुन्छ।';
+$lang['WALLET_SEND'] = 'कोष पठाउनुहोस्:';
+$lang['WALLET_ADDRESS'] = 'ठेगाना';
+$lang['WALLET_AMOUNT'] = 'रकम';
+$lang['WALLET_SENDCONF'] = 'पठाउनुहोस्';
+$lang['WALLET_USERADDRESSES'] = 'तपाईका ठेगानाहरू:';
+$lang['WALLET_NEWADDRESS'] = 'नयाँ ठेगाना पाउनुहोस्';
+$lang['WALLET_QRCODE'] = 'QR कोड';
+$lang['WALLET_LAST10'] = 'अन्तिम १० लेनदेन:';
+$lang['WALLET_DATE'] = 'मिति';
+$lang['WALLET_TYPE'] = 'प्रकार';
+$lang['WALLET_FEE'] = 'शुल्क';
+$lang['WALLET_CONFS'] = 'कन्फ्स';
+$lang['WALLET_INFO'] = 'जानकारी';
+
+?>
